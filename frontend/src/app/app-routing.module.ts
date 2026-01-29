481affab25e4c05556cce64ddfed7c3b69e7a0ed
Vincenzo Lamanuzzi <vinni@lamanuzzi.de>
2026-01-29 07:55:51 +0100

    Revert "Admin link added"
    This reverts commit cf1649b61e5f2f1fd0977ed70d72b0148907dc68.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import { HistoryComponent } from './history/history.component';
 import { AnalyzeComponent } from './analyze/analyze.component';
 import { HardwareDetailsComponent } from './hardware-details/hardware-details.component'; 
 import { HistoryDetailsComponent } from './history-details/history-details.component';
-import { ResultComponent } from './result/result.component';
-import { AdminComponent } from './admin/admin.component'; 
+import { ResultComponent } from './result/result.component';  
 
 const routes: Routes = [ { path: '', component: HomeComponent},
   { path: 'history', component: HistoryComponent},
@@ -14,7 +13,6 @@ const routes: Routes = [ { path: '', component: HomeComponent},
   { path: 'hardwareDetails', component: HardwareDetailsComponent},
   { path: 'history/details', component: HistoryDetailsComponent},
   { path: 'analyze/result', component: ResultComponent},
-  { path: 'admin', component: AdminComponent},
   
 ];
 
